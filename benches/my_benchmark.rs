d8ba362d139757360df2ea8529ad9923091bbbc6
Matheus-git <mathiew0@gmail.com>
2025-05-24 18:12:29 -0300

    feat: move tests

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,17 +1,56 @@
-use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+fn bench_dummy(c: &mut Criterion) {
+    c.bench_function("noop", |b| b.iter(|| {}));
+}
+
+criterion_group!(benches, bench_dummy);
+criterion_main!(benches);
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_minimo() {
+        let a = vec![3, 5, 2, 1, 7, 8, 4];
+        assert_eq!(minimo::minimo(&a, 7), 1);
+    }
+
+    #[test]
+    fn test_maximo() {
+        let a = vec![3, 5, 2, 1, 7, 8, 4];
+        assert_eq!(maximo::maximo(&a, 7), 8);
+    }
+
+    #[test]
+    fn test_heapsort() {
+        let mut a = vec![3, 5, 2, 1, 7, 8, 4];
+        heap::heapsort(&mut a);
+        assert_eq!(a, vec![1, 2, 3, 4, 5, 7, 8]);
+    }
+
+    #[test]
+    fn test_quicksort() {
+        let mut a = vec![3, 5, 2, 1, 7, 8, 4];
+        quicksort::quicksort(&mut a, 0, 6);
+        assert_eq!(a, vec![1, 2, 3, 4, 5, 7, 8]);
+    }
+
+    #[test]
+    fn test_counting_sort() {
+        let mut a = vec![3, 5, 2, 1, 7, 8, 4];
+        counting_sort::counting_sort(&mut a);
+        assert_eq!(a, vec![1, 2, 3, 4, 5, 7, 8]);
+    }
+
+    #[test]
+    fn test_pilha() {
+        assert!(pilha::pilha_vazia(&vec![]));
+        let mut a = vec![3, 5, 2, 1, 7, 8, 4];
+        pilha::push(&mut a, 10);
+        assert_eq!(a[a.len() - 1], 10);
+        pilha::pop(&mut a);
+        assert_eq!(pilha::topo(&a), 4);
     }
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-}
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,7 @@
-mod heap;
-mod quicksort;
-mod counting_sort;
-mod minimo;
-mod maximo;
-mod pilha;
-mod linked_list;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_minimo() {
-        let a = vec![3,5,2,1,7,8,4];
-        assert_eq!(minimo::minimo(&a, 7), 1);
-    }
-
-    #[test]
-    fn test_maximo() {
-        let a = vec![3,5,2,1,7,8,4];
-        assert_eq!(maximo::maximo(&a, 7), 8);
-    }
-
-    #[test]
-    fn test_heapsort() {
-        let mut a = vec![3,5,2,1,7,8,4];
-        heap::heapsort(&mut a);
-        assert_eq!(a, vec![1,2,3,4,5,7,8]);
-    }
-
-    #[test]
-    fn test_quicksort() {
-        let mut a = vec![3,5,2,1,7,8,4];
-        quicksort::quicksort(&mut a, 0, 6);
-        assert_eq!(a, vec![1,2,3,4,5,7,8]);
-    }
-
-    #[test]
-    fn test_couting_sort() {
-        let mut a = vec![3,5,2,1,7,8,4];
-        counting_sort::counting_sort(&mut a);
-        assert_eq!(a, vec![1,2,3,4,5,7,8]);
-    }
-
-    #[test]
-    fn test_pilha() {
-        assert!(pilha::pilha_vazia(&vec![]));
-        let mut a = vec![3,5,2,1,7,8,4];
-        pilha::push(&mut a, 10);
-        assert_eq!(a[a.len()-1], 10);
-        pilha::pop(&mut a);
-        assert_eq!(pilha::topo(&a), 4);
-    }
-
-    #[test]
-    fn test_linked_list() {
-        //TO DO
-    }
-    
-}
-
+pub mod heap;
+pub mod quicksort;
+pub mod counting_sort;
+pub mod minimo;
+pub mod maximo;
+pub mod pilha;
+pub mod linked_list;
