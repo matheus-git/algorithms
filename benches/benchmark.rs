f9957b07f347771ae734008395eba49354482138
Matheus-git <mathiew0@gmail.com>
2025-06-07 19:32:20 -0300

    feat: rod 40

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,21 +2,26 @@ use algorithms::rod_cutting::{rod_cutting, rod_cutting_naive};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench_dummy(c: &mut Criterion) {
+    let prices: Vec<i32> = (1..=40).map(|x| x * 2).collect();
+    let length = 40;
+
     let mut group = c.benchmark_group("Rod Cutting");
 
-    let prices = vec![1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80];
-    let length = 20;
+    group.bench_function("rod_dynamic", |b| {
+        b.iter(|| {
+            rod_cutting(&prices, length)
+        })
+    });
 
-    group.bench_function("rod_dynamic", |b| b.iter(|| {
-        rod_cutting(&prices, length)
-    }));
-
-    group.bench_function("rod_naive", |b| b.iter(|| {
-        rod_cutting_naive(&prices, length)
-    }));
+    group.bench_function("rod_naive", |b| {
+        b.iter(|| {
+            rod_cutting_naive(&prices, length)
+        })
+    });
 
     group.finish();
 }
+
 
 criterion_group!(benches, bench_dummy);
 criterion_main!(benches);
